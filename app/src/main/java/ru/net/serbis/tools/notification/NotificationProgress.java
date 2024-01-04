d178ef7ac7fe85e0004b3a379d5c159b1a9fef38
garbarick <serbis@gmail.com>
2024-01-04 23:15:30 +0400

    fixed notification on android 6

--- app/src/main/java/ru/net/serbis/tools/notification/NotificationProgress.java
+++ app/src/main/java/ru/net/serbis/tools/notification/NotificationProgress.java
@@ -2,6 +2,7 @@ package ru.net.serbis.tools.notification;
 
 import android.app.*;
 import android.content.*;
+import android.os.*;
 import java.util.*;
 import ru.net.serbis.tools.*;
 import ru.net.serbis.tools.util.*;
@@ -13,13 +14,21 @@ public class NotificationProgress extends Notification.Builder
 
     public NotificationProgress(Context context, int textId)
     {
-        super(context, context.getResources().getString(R.string.progress));
+        super(context);
+
         setSmallIcon(R.drawable.app);
         setContentText(context.getResources().getString(textId));
         String channelId = context.getResources().getString(R.string.progress);
-        NotificationChannel channel = new NotificationChannel(channelId, channelId, NotificationManager.IMPORTANCE_LOW);
+        setContentTitle(channelId);
+
         manager = SysTool.get().getService(context, Context.NOTIFICATION_SERVICE);
-        manager.createNotificationChannel(channel);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            setChannelId(channelId);
+            NotificationChannel channel = new NotificationChannel(channelId, channelId, NotificationManager.IMPORTANCE_LOW);
+            manager.createNotificationChannel(channel);
+        }
     }
 
     public void setProgress(int progress)
